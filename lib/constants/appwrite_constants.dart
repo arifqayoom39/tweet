abe7b22b765910dc8c427463835232271c73a422
Arif Qayoom <arifqayoom39@gmail.com>
2024-09-06 10:38:36 +0530

    first commit

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,11 +1,11 @@
 class AppwriteConstants {
-  static const String databaseId = '64293d218fa920a297a1';
-  static const String projectId = '64293b9272744e97c9d2';
+  static const String databaseId = 'databaseId';
+  static const String projectId = 'tweet';
   static const String endPoint = 'https://cloud.appwrite.io/v1';
 
-  static const String usersCollection = '66d473b6001e036007aa';
-  static const String tweetsCollection = '66d478760004fbfce369';
-  static const String notificationsCollection = '66d48dbd0029c287510f';
+  static const String usersCollection = 'users';
+  static const String tweetsCollection = 'tweets';
+  static const String notificationsCollection = 'notifications';
 
   static const String imagesBucket = '650923b438f43cbaba25';
 
